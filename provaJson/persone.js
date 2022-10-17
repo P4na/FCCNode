4e4005ee39290ea24d95fd2e311466cb0a33afe7
P4na <Vip_P4na@protonmail.com>
2022-10-17 13:17:45 +0200

    .

--- provaJson/persone.js
+++ provaJson/persone.js
@@ -34,6 +34,42 @@ const persone = [
             cap : "123456"
         },
         interessi: ["canne","figa"]
+    },
+    {
+    id : "3",
+    nome:"Luca",
+    cognome:"Rossi",
+    eta: 25,
+    indirizzo : {
+        citta: "Milano",
+        indirizzo: "Via le mani dal naso",
+        cap : "71016"
+        },
+    interessi: ["basket","figa"]
+    }, 
+    {
+        id : "4",
+        nome:"Gino",
+        cognome:"Lisa",
+        eta: 25,
+        indirizzo : {
+            citta: "Torino",
+            indirizzo: "Via le mani dal naso",
+            cap : "123456"
+            },
+        interessi: ["canne","figa"]
+    },
+    {
+        id : "5",
+        nome:"Signor",
+        cognome:"Minchiari",
+        eta: 69,
+        indirizzo : {
+            citta: "Belin",
+            indirizzo: "Via Di Estinzione",
+            cap : "123456"
+            },
+        interessi: ["canne","figa"]
     }
 ]
 
